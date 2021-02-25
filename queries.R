cb8f09ccc7f8941a1f423306ae5e4db19be495f5
estefls <estef_ls@live.com.mx>
2021-02-25 03:56:02 +0000

    Packages were installed

--- queries.R
+++ queries.R
@@ -1,1 +1,19 @@
 # Sesión 7 Reto 01
+
+
+install.packages(DBI)
+install.packages("RMySQL")
+install.packages("dplyr")
+
+
+library (DBI)
+library(RMySQL)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+library(dplyr)
